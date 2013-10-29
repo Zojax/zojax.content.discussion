3c75403bd07656f616bd04e196feb4718403df76
Dmitry Ivanchenko <dmitry.ivanchenko@zojax.com>
2013-10-29 18:05:03 +0200

    549 fix condition

--- src/zojax/content/discussion/comment.py
+++ src/zojax/content/discussion/comment.py
@@ -37,7 +37,6 @@ from interfaces import ISimpleComment, IThreadedComment, IContentDiscussion, ICo
 
 from utils import getAthorFromCookie
 
-
 class Comment(Persistent, Location):
     interface.implements(ISimpleComment, IThreadedComment, ISocialComment)
 
@@ -110,29 +109,19 @@ class Comment(Persistent, Location):
         """ visible with the approval
         """
         content = self.content
-        request = getRequest()
-        cookieAuthor = getAthorFromCookie(request)
-
-        print 'IContentDiscussion(content).status: ', IContentDiscussion(content).status
-        print 'self.authorName: ', self.authorName
-        print 'cookieAuthor: ', cookieAuthor
-        print 'self.social_type: ', self.social_type
-        print 'self.approved: ', self.approved
-        print "getattr(self, 'facebook_id', ''): ", getattr(self, 'facebook_id', '')
-        print "request.get('facebook_id', None): ", request.get('facebook_id', None)
-        print "request.get('screen_name'): ", request.get('screen_name')
-
         if IContentDiscussion(content).status == 4:
             if not checkPermission('zojax.ModifyContent', content):
-                #request = getRequest()
-                #cookieAuthor = getAthorFromCookie(request)
+                request = getRequest()
+                cookieAuthor = getAthorFromCookie(request)
                 if cookieAuthor == self.authorName:
                     return True
-                if 'social_type' in dir(self) and self.social_type:
-                    if getattr(self, 'facebook_id', '') == request.get('facebook_id', None) or \
-                                    self.authorName == request.get('screen_name'):
-                                    # self.authorName == urllib.unquote(getattr(request, 'screen_name', '')):
+                if getattr(self, 'social_type', False):
+                    facebook_id = request.get('facebook_id', None)
+                    if self.authorName == request.get('screen_name'):
                         return True
+                    if facebook_id is not None:
+                        if getattr(self, 'facebook_id', '') == facebook_id:
+                            return True
                 return self.approved
 
         return True
